ca77ea429c66ff89f28909eda110f9b78543d61e
simon-bursa <146720121+simon-bursa@users.noreply.github.com>
2023-11-20 13:20:38 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
+let isPressedA = 0
+let randomNumber = 0
+let isPressedB = 0
+let counter = 0
+let counter_hold = 0
+let waitTime = 12
+
 input.onButtonPressed(Button.A, function () {
     counter_hold = 0
-    counter += 1
+    if (counter < 99) {
+        counter += 1
+        whaleysans.showNumber(counter)
+    }
 })
+
+input.onButtonPressed(Button.B, function () {
+    counter_hold = 0
+    if (counter > 0) {
+        counter += -1
+        whaleysans.showNumber(counter)
+    }
+})
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = Math.floor(Math.random() * counter)
+    whaleysans.showNumber(randomNumber)
+    basic.pause(2500)
+})
+
 function handleButton () {
-    if (input.buttonIsPressed(Button.A) && isPressedB == 0) {
+    if (input.buttonIsPressed(Button.A) && isPressedB == 0 && counter < 99) {
         counter += 1
     }
     if (input.buttonIsPressed(Button.B) && isPressedB == 1 && counter > 0) {
@@ -12,19 +37,9 @@ function handleButton () {
     if (input.buttonIsPressed(Button.AB)) {
         counter = 0
     }
+    whaleysans.showNumber(counter)
 }
-input.onButtonPressed(Button.B, function () {
-    counter_hold = 0
-    if (counter > 0) {
-        counter += -1
-    }
-})
-let isPressedA = 0
-let isPressedB = 0
-let counter_hold = 0
-let counter = 0
-counter = 0
-let waitTime = 12
+
 basic.forever(function () {
     basic.pause(20)
     serial.writeLine("" + (counter))
